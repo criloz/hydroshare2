7d81dc960035fe1b47c0fe5bbcdcb188bf6a8876
Jefferson Heard <jeff@renci.org>
2014-06-20 14:03:59 +0000

    current beta site

--- local_settings.py
+++ local_settings.py
@@ -79,3 +79,10 @@ HYDROSHARE_APPS = (
     "hs_core"
 )
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'hydroshare@hydroshare.org'
+EMAIL_HOST_PASSWORD = 'zR=D~QBxU&}+'
+EMAIL_HOST = 'gator3038.hostgator.com'
+EMAIL_PORT = '26'
+EMAIL_USE_TLS= True
+DEFAULT_FROM_EMAIL= 'hydroshare@hydroshare.org'
